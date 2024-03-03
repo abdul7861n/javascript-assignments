f8d4c5af45720c0e45f9d04a8bc90b4a916e5753
abdul7861n <155259858+abdul7861n@users.noreply.github.com>
2024-03-03 15:46:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,102 @@
-//1. Write a script to greet your website visitor using JS alert box
+//chapter variables for strings start
 
-alert("Hello! Welcome to our website.");
+//1. Declare a variable called username.
 
-//2. Write a script to display following message on your web page
+var username = "your_username_here";
+console.log(username);
 
-alert("Error! Please Enter a Valid Password");
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
 
-//3. Write a script to display following message on your web page
+var myName = "Abdullah Nadeem";
+console.log(myName);
 
-alert("Welocome To JS Land.....\n Happy Coding");
+//3. Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
 
-//4. Write a script to display following messages in sequence:
+var message;
+var message="Hello World";
+alert(message);
 
-alert("Welocme To Java script");
+//4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
 
-alert("Happy Coding");
+var firstName = "Abdullah";
+var lastName = "Nadeem";
+var age = 20;
+var grade = "A";
 
-//5. Generate the following message through browser’s developer console
+alert("First Name: " + firstName);
+alert("Last Name: " + lastName);
+alert("Age: " + age);
+alert("Grade: " + grade);
 
-alert("Hello.... I Can Run JS Through my web browser console");
+//5. Write a script to display the following alert using one JS variable:
 
-console.log("Hello.... I Can Run JS Through my web browser console");
+var alertMessage= "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(alertMessage);
 
-//6. Make use of alerts in your new/existing HTML & CSS project.
+//6. Declare a variable called email and assign to it a string that
+//represents your Email Address(e.g. example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: use
+//string concatenation)
 
-function showAlert() {
-    alert("This is an alert! Hello from JavaScript.");
-}
+var email = "example@example.com";
+alert("My email address is: " + email);
 
+//7. Declare a variable called book & give it the value “A
+//smarter way to learn JavaScript”. Display the following
+//message in an alert box:
 
+var book = "A smarter way to learn JavaScript";
+alert("I am currently reading the book: " + book);
+
+//8. Write a script to display this in browser through JS
+
+var textToDisplay ="Yah! I Can Write HTML content through JavaScript <br/>";
+document.write(textToDisplay);
+
+//9. Store following string in a variable and show in alert and browser through JS
+
+var message ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(message);
+
+//chapter variables for string end
+
+//chapter variables for numbers start
+
+//1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+var age ="25";
+alert("My Age is: " + age);
+
+//2. Declare & initialize a variable to keep track of how many
+//times a visitor has visited a web page. Show his/her
+//number of visits on your web page. For example: “You
+//have visited this site N times”.
+
+var times ="14 times";
+alert("You have visited the site: " +times);
+
+//3. Declare a variable called birthYear & assign to it your
+//birth year. Show the following message in your browser:
+
+var birthYear = "2003 <br/>";
+document.write("My birthYear is:" + birthYear);
+
+//4. A visitor visits an online clothing store
+//www.xyzClothing.com . Write a script to store in variables
+//the following information:
+//a. Visitor’s name
+//b. Product title
+//c. Quantity i.e. how many products a visitor wants to
+//order
+//Show the following message in your browser: “John
+//Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + productTitle + "(s) on XYZ Clothing store.");
+
+//chapter variables for numbers end 
