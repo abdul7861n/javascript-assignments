fe0fbbe9c4dae6e9a82c52a8018d73f3413ed036
abdul7861n <155259858+abdul7861n@users.noreply.github.com>
2024-03-03 15:55:00 +0500

    Add files via upload

---
+++ javascript assignments/app.js
@@ -0,0 +1,31 @@
+//1. Write a script to greet your website visitor using JS alert box
+
+alert("Hello! Welcome to our website.");
+
+//2. Write a script to display following message on your web page
+
+alert("Error! Please Enter a Valid Password");
+
+//3. Write a script to display following message on your web page
+
+alert("Welocome To JS Land.....\n Happy Coding");
+
+//4. Write a script to display following messages in sequence:
+
+alert("Welocme To Java script");
+
+alert("Happy Coding");
+
+//5. Generate the following message through browser’s developer console
+
+alert("Hello.... I Can Run JS Through my web browser console");
+
+console.log("Hello.... I Can Run JS Through my web browser console");
+
+//6. Make use of alerts in your new/existing HTML & CSS project.
+
+function showAlert() {
+    alert("This is an alert! Hello from JavaScript.");
+}
+
+
